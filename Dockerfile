c726b9cecdcbc9959112b76e76772c58f9e5d579
Joshua Azimullah <j.r.azimullah@student.tudelft.nl>
2025-09-20 11:35:36 +0200

    Sat Sep 20 11:35:36 CEST 2025

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,18 @@
-FROM ubuntu:24.04
+FROM python:3.11-slim
+
+ENV PYTHONUNBUFFERED=1 \
+    PIP_NO_CACHE_DIR=1
+
+RUN apt-get update && \
+    apt-get install -y --no-install-recommends \
+        ffmpeg \
+        ca-certificates \
+        && rm -rf /var/lib/apt/lists/*
+
+RUN pip install yt-dlp==2025.8.11
 
 WORKDIR /app
 
-RUN apt-get update && \
-    apt-get install -y curl ca-certificates python3 && \
-    rm -rf /var/lib/apt/lists/*
-
-RUN curl -L https://github.com/yt-dlp/yt-dlp/releases/download/2025.09.05/yt-dlp_linux_aarch64 -o yt-dlp
-RUN chmod +x yt-dlp
-
 COPY main.py main.py
 
-CMD ["tail", "-f", "/dev/null"]
+CMD ["bash"]
--- main.py
+++ main.py
@@ -1,13 +1,45 @@
-# import yt_dlp
+import yt_dlp
 
-# ydl_opts = {
-#         'output': "%(title)s-%(id)s.%(ext)s"
-#         'restrict-filenames'
-# }
+class MyLogger:
+    def debug(self, msg):
+        # For compatibility with youtube-dl, both debug and info are passed into debug
+        # You can distinguish them by the prefix '[debug] '
+        if msg.startswith('[debug] '):
+            print(msg)
+            pass
+        else:
+            self.info(msg)
 
-# URLS = ['https://www.youtube.com/watch?v=apREl0KmTdQ']
-# with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-#     ydl.download(URLS)
+    def info(self, msg):
+        print(msg)
+        pass
+
+    def warning(self, msg):
+        print(msg)
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+
+# ℹ️ See "progress_hooks" in help(yt_dlp.YoutubeDL)
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now post-processing ...')
+
+ydl_opts = {
+        'output': "%(title)s-%(id)s.%(ext)s",
+        'restrictfilenames': True,
+        'logger': MyLogger(),
+        'progress_hooks': [my_hook],
+}
+
+
+
+URLS = ['https://www.youtube.com/watch?v=apREl0KmTdQ']
+print("Starting download")
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(URLS)
 
 
 
